998e26744fff1bad81bf653690ff02101c1424ea
Elliot Scott Chan <elliotscottchan@gmail.com>
2026-03-31 15:50:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOi
 ========================= */
 // Grant CesiumJS access to your ion assets
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MDU4NWI1YS03ZGUxLTRmMzEtODEwZi01MDNlM2QyMTg5MzAiLCJpZCI6NDExNTkzLCJpYXQiOjE3NzQ5MjgxNjh9.NeKegq8BpQ4KqIs2hJWNgoEy2c0vidgNg869ldUVFew";
-
 const viewer = new Cesium.Viewer("cesiumContainer", {
- geocoder: true,
-    baseLayerPicker: true,
+  geocoder: true,
+  baseLayerPicker: true
 });
 
 async function loadImagery() {
